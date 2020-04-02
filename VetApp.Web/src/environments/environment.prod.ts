4e4417887c264a4c672f6c2809fdb3c6d9d52587
Diego Felipe Jaramillo <dief.jara@hotmail.com>
2020-04-01 20:35:02 -0500

    initial commit

---
+++ VetApp.Web/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9PQ8G6lnA1HxYK05wg_z-U3KAV-y20J4",
+    authDomain: "dofapp-ae269.firebaseapp.com",
+    databaseURL: "https://dofapp-ae269.firebaseio.com",
+    projectId: "dofapp-ae269",
+    storageBucket: "dofapp-ae269.appspot.com",
+    messagingSenderId: "444556882488"
+  }
+  };
---
+++ VetApp.Web/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD9PQ8G6lnA1HxYK05wg_z-U3KAV-y20J4",
+    authDomain: "dofapp-ae269.firebaseapp.com",
+    databaseURL: "https://dofapp-ae269.firebaseio.com",
+    projectId: "dofapp-ae269",
+    storageBucket: "dofapp-ae269.appspot.com",
+    messagingSenderId: "444556882488"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
